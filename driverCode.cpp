db196915f92492e9f674e2d63a0c5f1a2e1ba8ca
r-hari-shankar <65308865+r-hari-shankar@users.noreply.github.com>
2021-02-24 12:52:02 +0530

    Saves the required traffic and empty images

--- driverCode.cpp
+++ driverCode.cpp
@@ -53,10 +53,20 @@ int main( int argc, char** argv)
     cv::imshow("SelectedPoints", inputImage);
     cv::Mat angleCorrectedImage = changePerspective(originalImage, points);
     cv::imshow("New Perspective", angleCorrectedImage);
-    cv::waitKey(0);
-    cv::imwrite("perspective.jpg",angleCorrectedImage);
+    int n = cv::waitKey(0);
+    if(n=='t'){
+        cv::imwrite("perspective_traffic.jpg",angleCorrectedImage);
+    }
+    else{
+        cv::imwrite("perspective_empty.jpg",angleCorrectedImage);
+    }
     cv::Mat croppedImage = cropImage(angleCorrectedImage);
     cv::imshow("New Perspective + Cropped", croppedImage);
-    cv::waitKey(0);
-    cv::imwrite("cropped.jpg",croppedImage);
+    int k = cv::waitKey(0);
+    if(k=='t'){
+        cv::imwrite("cropped_traffic.jpg",croppedImage);
+    }
+    else{
+        cv::imwrite("cropped_empty.jpg",croppedImage);
+    }
 }
