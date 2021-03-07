7e401ce0ca4a686f17d34455c26ef74ca3fcf413
dishantdhiman <66732036+dishantdhiman@users.noreply.github.com>
2021-03-08 02:04:53 +0530

    file with queue and dynamic density functions

---
+++ bgEditor.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<opencv2/opencv.hpp>
+#include "bgEditor.hpp"
+
+cv::Mat backgroundSubtraction(cv::Mat img) {
+    cv::Mat mask;
+    cv::BackgroundSubtractorMOG2 remover;
+    remover.apply(img, mask);
+    return mask;
+}
+
+double getQueue(cv::Mat img, cv::Mat emptyImage) {
+    cv::Mat img1 = backgroundSubtraction(img);
+    cv::Mat img2 = backgroundSubtraction(emptyImage);
+
+    int a1 = cv::countNonZero(img1);
+    int a2 = cv::countNonZero(img2);
+
+    return (double)(a1-a2) / (double)(img1.rows*img1.cols);
+}
+
+double getDynamic(cv::Mat img, cv::Mat previousImage) {
+    cv::Mat img1 = backgroundSubtraction(img);
+    cv::Mat img2 = backgroundSubtraction(previousImage);
+
+    int a1 = cv::countNonZero(img1);
+    int a2 = cv::countNonZero(img2);
+
+    return (double)(a1-a2) / (double)(img1.rows*img1.cols);
+}
