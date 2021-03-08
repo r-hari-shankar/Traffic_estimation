fdb257cdb3bdbdb8b47a255f6953c64e57d6971c
dishantdhiman <66732036+dishantdhiman@users.noreply.github.com>
2021-03-08 18:56:46 +0530

    changing the implementation

--- bgEditor.cpp
+++ bgEditor.cpp
@@ -3,29 +3,24 @@
 #include<opencv2/opencv.hpp>
 #include "bgEditor.hpp"
 
-cv::Mat backgroundSubtraction(cv::Mat img) {
-    cv::Mat mask;
-    cv::Ptr<cv::BackgroundSubtractor> pBackSub;
-    pBackSub->apply(img, mask);
-    return mask;
+cv::Mat backgroundSubtraction(cv::Mat img, cv::Mat mask) {
+    cv::Mat output;
+    cv::subtract(img, mask, output);
+    return output;
 }
 
 double getQueue(cv::Mat img, cv::Mat emptyImage) {
-    cv::Mat img1 = backgroundSubtraction(img);
-    cv::Mat img2 = backgroundSubtraction(emptyImage);
+    cv::Mat img1 = backgroundSubtraction(img, emptyImage);
 
     int a1 = cv::countNonZero(img1);
-    int a2 = cv::countNonZero(img2);
 
-    return (double)(a1-a2) / (double)(img1.rows*img1.cols);
+    return (double)(a1) / (double)(img1.rows*img1.cols);
 }
 
 double getDynamic(cv::Mat img, cv::Mat previousImage) {
-    cv::Mat img1 = backgroundSubtraction(img);
-    cv::Mat img2 = backgroundSubtraction(previousImage);
+    cv::Mat img1 = backgroundSubtraction(img, previousImage);
 
     int a1 = cv::countNonZero(img1);
-    int a2 = cv::countNonZero(img2);
 
-    return (double)(a1-a2) / (double)(img1.rows*img1.cols);
+    return (double)(a1) / (double)(img1.rows*img1.cols);
 }
