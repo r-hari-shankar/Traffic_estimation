fd1377e7dcefa3b39ffe2806a278c7d71c40eec8
r-hari-shankar <65308865+r-hari-shankar@users.noreply.github.com>
2021-03-10 04:04:31 +0530

    Error handling and comments added

--- video.cpp
+++ video.cpp
@@ -14,53 +14,69 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    const char* input_file = "{ input          | argv[1] |}";
+    //To check if input is null
+    if(argv[1]==NULL){
+        std::cout << "Video input not given ,check readme file for help" << std::endl;
+        return 1;
+    }
+    //To check if the video input file does not exist
+    ifstream ifile;
+    ifile.open(argv[1]);
+    if(!ifile) {
+        cout<<"Video file doesn't exist in current directory"<<"\n";
+        return 0;  
+    }
+    //opening the video file
     string instream = argv[1];
-    CommandLineParser parser(argc, argv, input_file);
-    parser.about( "This program shows how to use background subtraction methods provided by "
-                  " OpenCV. You can process both videos and images.\n" );
-    
     VideoCapture capture( samples::findFile( instream ) );
     if (!capture.isOpened()){
         cerr << "Unable to open: " << instream << endl;
         return 0;
     }
     cv::Mat frame;
-    cv::Mat empty = cv::imread("cropped_road.png",cv::IMREAD_GRAYSCALE);
-    cv::Mat previous = empty; 
-    //imshow("pic",empty);
+    cv::Mat empty = cv::imread("cropped_road.png",cv::IMREAD_GRAYSCALE); //empty frame for queue density evaluation
+    cv::Mat previous = empty; //This matrix will store the value of the previous frame
+ 
+    //points specify the region which is angle corrected and cropped
     std::vector<cv::Point2f> points;
     points.push_back(Point2f(977,212));
     points.push_back(Point2f(311,1059));
     points.push_back(Point2f(1530,1058));
     points.push_back(Point2f(1264,210));
     cout<<"frame_number"<<" "<<"queue density"<<" "<<"dynamic density"<<"\n";
+
+    //fstream used to append the output densities in the "out.txt" file
     fstream file;
     file.open("out.txt",ios::out | ios::in);
-    int frame_number=0;
-    while (true) {
+    int frame_number=0; //keeps count of the frame
+    while (true) {  //loop for traversing through all the frames of the video file
         capture >> frame;
         cv:: Mat grayscale;
         if (frame.empty())
             break;
+        //size of frame discribed by rectangle
         rectangle(frame, cv::Point(10, 2), cv::Point(100,20),
                   cv::Scalar(255,255,255), -1);
+        //To change color to grayscale
         cvtColor(frame, grayscale, COLOR_RGB2GRAY);
+        //for frame number of the playing video
         stringstream ss;
         ss << capture.get(CAP_PROP_POS_FRAMES);
         string frameNumberString = ss.str();
         putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
                 FONT_HERSHEY_SIMPLEX, 0.5 , cv::Scalar(0,0,0));
+        //angle corrected and cropped image produced using functions defined in the previous assignment
         cv::Mat angleCorrectedImage = changePerspective(grayscale, points);
         cv::Mat croppedImage = cropImage(angleCorrectedImage);
-        double queue = getQueue(croppedImage,empty);
-        double dynamic = getDynamic(croppedImage,previous);
-        cout<<frame_number<<" "<<queue<<" "<<dynamic<<"\n";
-        file<<frame_number<<" "<<queue<<" "<<dynamic<<"\n";
-        previous = croppedImage;
-        imshow("Density", croppedImage); 
+        double queue = getQueue(croppedImage,empty); //stores the queue density
+        double dynamic = getDynamic(croppedImage,previous); //stores the dynamic density
+        cout<<frame_number<<" "<<queue<<" "<<dynamic<<"\n"; //return the values in console
+        file<<frame_number<<" "<<queue<<" "<<dynamic<<"\n"; //return the values in the "out.txt"
+        previous = croppedImage; //so that previous points to the previous frame 
+        imshow("Density", croppedImage); //video displayed
         frame_number++;
-        int keyboard = waitKey(30);
+        //Press any key to exit the program
+        int keyboard = waitKey(30); 
         if (keyboard >= 65 && keyboard <= 122 || keyboard == 27)
             break;
     }
